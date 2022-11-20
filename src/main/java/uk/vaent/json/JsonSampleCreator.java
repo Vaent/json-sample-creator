cd3a158b7d2d5d491b62ac1772ad13cda2feae12
Vaent <gh@vaent.uk>
2022-11-20 17:38:26 +0000

    Produce constant output for single-value types

--- src/main/java/uk/vaent/json/JsonSampleCreator.java
+++ src/main/java/uk/vaent/json/JsonSampleCreator.java
@@ -1,7 +1,26 @@
 package uk.vaent.json;
 
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class JsonSampleCreator {
     public static void main(String... args) {
-        System.out.println("{\n  \"Hello\": \"JSON\"\n}");
+        Pattern typeFinder = Pattern.compile("\"type\"\s*:\s*\"([^\"]*)\"");
+        Matcher typeMatch = typeFinder.matcher(args[0]);
+        if (typeMatch.find()) {
+            String type = typeMatch.group(1).toLowerCase();
+            switch (type) {
+                case "boolean":
+                    System.out.println(true);
+                    break;
+                case "number":
+                    System.out.println(42);
+                    break;
+                case "string":
+                    System.out.println("\"Hello JSON\"");
+                    break;
+            }
+        }
     }
 }
---
+++ src/test/java/uk/vaent/json/JsonSampleCreatorTest.java
@@ -0,0 +1,54 @@
+package uk.vaent.json;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JsonSampleCreatorTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputListener = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputListener));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void testCreateBoolean() {
+        String schema = "{\"type\":\"boolean\"}";
+        JsonSampleCreator.main(schema);
+        String output = outputListener.toString().strip();
+        assertFalse(output.matches("\".*\""), "Boolean should not be represented as a string");
+        assertTrue("true".equalsIgnoreCase(output) || "false".equalsIgnoreCase(output));
+    }
+
+    @Test
+    public void testCreateNumber() {
+        String schema = "{\"type\":\"number\"}";
+        JsonSampleCreator.main(schema);
+        String output = outputListener.toString().strip();
+        assertFalse(output.matches("\".*\""), "Number should not be represented as a string");
+        try {
+            Double.parseDouble(output);
+        } catch (NumberFormatException ex) {
+            fail("Output was non-numeric");
+        }
+    }
+
+    @Test
+    public void testCreateString() {
+        String schema = "{\"type\":\"string\"}";
+        JsonSampleCreator.main(schema);
+        String output = outputListener.toString().strip();
+        assertTrue(output.matches("^\".*\"$"));
+    }
+}
