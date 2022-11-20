d883cc0e9cd2c61ba823731e2e1bad6b7b820c2d
Vaent <gh@vaent.uk>
2022-11-20 16:15:05 +0000

    Initialise project

---
+++ README.md
@@ -0,0 +1,3 @@
+# json-sample-creator
+
+A tool for generating sample JSON data based on user-supplied schemas and parameters.
---
+++ src/main/java/uk/vaent/json/JsonSampleCreator.java
@@ -0,0 +1,7 @@
+package uk.vaent.json;
+
+public class JsonSampleCreator {
+    public static void main(String... args) {
+        System.out.println("{\n  \"Hello\": \"JSON\"\n}");
+    }
+}
